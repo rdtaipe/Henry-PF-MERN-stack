02b69fe2c18a7479af7db3338288d7e6869b8a43
DrewAce <juancarmona3090@gmail.com>
2023-04-11 15:31:11 -0500

    Modules

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+import server from "./app.js";
+// import net from 'net';
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+async function startServer() {
+  const port = process.env.SERVER_PORT || 5000;
+  await server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
   });
-});
+}
+startServer()
--- src/db.js
+++ src/db.js
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+var {DB_URI}=process.env||{DB_URI:"mongodb://localhost:27017/test"}
+
+export const connect= async()=> {
+  await mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log("connect to mogodb atlas ")
+}
