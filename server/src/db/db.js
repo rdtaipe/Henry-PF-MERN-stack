5f013be90d34672cf06fef08af6d5915323c9e34
DrewAce <juancarmona3090@gmail.com>
2023-04-11 16:08:35 -0500

    FolderChange

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -10,5 +10,5 @@ export const connect= async()=> {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  console.log("connect to mogodb atlas ")
+  console.log("connect to mongodb atlas ")
 }
