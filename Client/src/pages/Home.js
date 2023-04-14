0c6ed7e7a69cc207f08fd3fa0701c1a317bd3b4d
rdt <77520484+rdtaipe@users.noreply.github.com>
2023-04-14 09:50:14 -0500

    please pass
    sadads

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Card from '../components/Card'
-import { useDispatch, useSelector } from 'react-redux'
 
 const Home = () => {
   //testing redux
-  const dispatch=useDispatch()
-  const setter=useSelector(state=>state.actions.setter)
-  const state=useSelector(state=>state)
-  console.log(state,"state")
+  const dispatch = useDispatch()
+  const setter = useSelector(state => state.actions.setter)
+  const state = useSelector(state => state)
+  console.log(state, "state")
   return (
     <div>
-        <Header/>
-        <Card/>
+      <Header />
+      <Card />
 
     </div>
   )
--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,6 +1,6 @@
 import {SET_STATE} from './types';
 
-export function setter(value) {
+export async function setState(value) {
     return {type:SET_STATE,payload:value}
 }
 
