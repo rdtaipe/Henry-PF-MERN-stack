ea7bdaa04a8006e30c9958c31472fc3f8cfa3a8f
Jacobo Cohello <j-acobo@hotmail.com>
2023-04-13 19:26:51 -0500

    sidebar

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import Card from '../components/Card'
+import Sidebar from '../components/Sidebar/Sidebar'
 
 const Home = () => {
   return (
     <div>
         <Header/>
-        <Card/>
+        <Sidebar/>
  
     </div>
   )
