9edee32e72f9c434e7442a9fc203dc49c20b094a
rdt <77520484+rdtaipe@users.noreply.github.com>
2023-04-14 09:57:46 -0500

    f
    f

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Card from '../components/Card'
 import { useDispatch, useSelector } from 'react-redux'
-
 const Home = () => {
   //testing redux
   const dispatch = useDispatch()
   const setter = useSelector(state => state.actions.setter)
   const state = useSelector(state => state)
   console.log(state, "state")
+
   return (
     <div>
       <Header />
