1a438f6ef685ac01ce5673c123b68fc1aff3abe6
Jacobo Cohello <j-acobo@hotmail.com>
2023-04-14 15:59:18 -0500

    carousel

--- Client/src/components/Sidebar/Sidebar.jsx
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -20,6 +20,8 @@ function Sidebar() {
     });
   };
 
+  console.log(selections)
+
   return (
     <div className="flex flex-col min-h-screen w-72 bg-stone-400">
 
@@ -28,19 +30,19 @@ function Sidebar() {
             <SidebarButton title="Marca" items={["Nike", "Adidas", "Puma", "Reebok"]} onSelect={(selectedItems) => handleSelection("Marca", selectedItems)} />
             <SidebarButton title="Color" items={["Rojo", "Azul", "Verde", "Negro", "Blanco"]} onSelect={(selectedItems) => handleSelection("Color", selectedItems)} />
             <SidebarButton title="Género" items={["Hombre", "Mujer", "Niño", "Niña"]} onSelect={(selectedItems) => handleSelection("Género", selectedItems)} />
-            <SidebarButton title="Precios" items={["20$ - 40$", "40$ - 60$", "60$ - 80$", "80$ - 100$", "> 100"]} onSelect={(selectedItems) => handleSelection("Precios", selectedItems)} />
+            <SidebarButton title="Precios" items={["20$ - 40$", "40$ - 60$", "60$ - 80$", "80$ - 100$", "> 100$"]} onSelect={(selectedItems) => handleSelection("Precios", selectedItems)} />
         </div>
 
         <button
-        className="py-2 px-4 bg-blue-500 text-white mt-12 hover:bg-blue-600"
-        onClick={async () => {
-            const response = await axios.get("https://example.com/api", {
-            params: selections,
-            });
-            console.log(response.data);
-        }}
+          className="py-2 px-4 bg-blue-500 text-white mt-12 hover:bg-blue-600"
+          onClick={async () => {
+              const response = await axios.get("https://example.com/api", {
+              params: selections,
+              });
+              console.log(response.data);
+          }}
         >
-        Apply filters
+          Apply filters
         </button>
 
     </div>
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Card from '../components/Card'
+import { useDispatch, useSelector } from 'react-redux'
 import NavBar from '../components/NavBar'
 import Sidebar from '../components/Sidebar/Sidebar'
 
 const Home = () => {
+  //testing redux
+  const dispatch = useDispatch()
+  const setter = useSelector(state => state.actions.setter)
+  const state = useSelector(state => state)
+  console.log(state, "state")
+
   return (
     <div>
       <NavBar />
