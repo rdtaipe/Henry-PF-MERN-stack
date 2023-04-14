f2e73bcbac18e1a93b06d74978fe9661fa23a5a2
Maximo Guzman <vmaximoguzman@gmail.com>
2023-04-14 13:55:46 -0300

    Estilos extra DetailPage

--- Client/src/components/DetailPage.jsx
+++ Client/src/components/DetailPage.jsx
@@ -17,6 +17,11 @@ export const DetailPage = () => {
     <div className="h-screen grid grid-cols-2 content-center justify-around bg-slate-200">
       <div className="flex justify-center">
         <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/018c0d32-59ec-47bc-9244-cd484bb45d1f/air-force-1-le-zapatillas-nino-a-r2kdvj.png" />
+        {/* {product.image
+          ? product.image.map((el) => {
+              return <img alt={product.name} src={el} key={el} />;
+            })
+          : null} */}
       </div>
       <div className="flex flex-col justify-center gap-10 w-9/12">
         <div>
@@ -26,18 +31,15 @@ export const DetailPage = () => {
         </div>
         <div className="flex flex-col">
           <p className="text-center pb-5">{product.description}</p>
-          {/* <p className="text-center pb-5">
-            El fulgor sigue vivo con las Nike Air Force 1 '07, un icono del
-            baloncesto que aporta un nuevo toque a su ya característica piel
-            impecable, sus colores llamativos y la cantidad perfecta de
-            reflectante.
-          </p> */}
 
           <p className="font-bold text-4xl pb-3">${product.price}</p>
 
           {/* <p>{product.stock}</p> */}
 
-          <p>Colores Disponibles: {product.color}</p>
+          <p>
+            <span className="font-bold">Colores Disponibles:</span>{" "}
+            {product.color}
+          </p>
 
           {/* <p>{product.genre}</p> */}
 
@@ -54,27 +56,15 @@ export const DetailPage = () => {
                   );
                 })
               : null}
-            {/* Cambiar visualización de size */}
-            {/* <p className="font-bold bg-white px-3 py-2 border-2 border-black rounded mx-auto cursor-pointer">
-              41
-            </p>
-            <p className="font-bold bg-white px-3 py-2 border-2 border-black rounded mx-auto cursor-pointer">
-              42
-            </p>
-            <p className="font-bold bg-white px-3 py-2 border-2 border-black rounded mx-auto cursor-pointer">
-              43
-            </p> */}
           </div>
 
           {/* <p>{product.brand ? product.brand.name : null}</p> */}
-        </div>
 
-        <button className="bg-purple-500 h-10 text-center border-2 border-black rounded hover:font-bold">
-          Comprar!
-        </button>
+          <button className="mt-10 font-semibold bg-purple-500 h-10 text-center border-2 border-black rounded hover:transform hover:scale-105 transition-all duration-500 text-white">
+            Agregar Al Carrito
+          </button>
+        </div>
       </div>
     </div>
   );
 };
-
-//#A78BFA
