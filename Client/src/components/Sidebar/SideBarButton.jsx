65c29d2fc62338dd0d6618de3dc05ee6d4a079ba
Jacobo Cohello <j-acobo@hotmail.com>
2023-04-16 19:27:44 -0500

    rework landing

--- Client/src/components/Sidebar/SideBarButton.jsx
+++ Client/src/components/Sidebar/SideBarButton.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 function SidebarButton({ title, items, onSelect }) {
-
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItems, setSelectedItems] = useState([]);
 
@@ -10,18 +9,16 @@ function SidebarButton({ title, items, onSelect }) {
     setIsOpen(!isOpen);
   };
 
-  const handleCheckboxChange = (event,item) => {
-  
+  const handleCheckboxChange = (event, item) => {
     const newSelectedItems = event.target.checked
       ? [...selectedItems, item]
       : selectedItems.filter((i) => i !== item);
     setSelectedItems(newSelectedItems);
-    onSelect && onSelect(newSelectedItems); // Se notifica al componente padre de los items seleccionados
+    onSelect && onSelect(newSelectedItems);
   };
 
   return (
     <div className="relative">
-      
       <button
         className="w-full py-4 px-6 bg-stone-700 text-white hover:bg-stone-600 focus:bg-black flex justify-between items-center pl-20"
         onClick={handleButtonClick}
@@ -32,19 +29,18 @@ function SidebarButton({ title, items, onSelect }) {
 
       {isOpen && (
         <div className="z-10 w-full py-2 bg-stone-300 shadow-lg flex flex-col pl-20 transition-all duration-300">
-
           {items.map((item) => (
             <label key={item} className="inline-flex items-center w-full px-2">
               <input
                 type="checkbox"
                 className="form-checkbox"
                 value={item}
-                onChange={e=>handleCheckboxChange(e,item)}
+                onChange={(e) => handleCheckboxChange(e, item)}
                 checked={selectedItems.includes(item)}
               />
-              <span className="ml-2 text-gray-700">{
-                title==="price"?item.toLocaleString("en-US", {style: "currency", currency: "USD"}):item
-              }</span>
+              <span className="ml-2 text-gray-700">
+                {typeof item === "number" ? item + "$" : item}
+              </span>
             </label>
           ))}
         </div>
@@ -54,3 +50,4 @@ function SidebarButton({ title, items, onSelect }) {
 }
 
 export default SidebarButton;
+
--- Client/src/components/Sidebar/Sidebar.jsx
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -7,9 +7,9 @@ const values=["category","brand","color","genre","price"]
 
 function Sidebar({className, setFilter}) {
   const dispatch = useDispatch()
-  const {top, width}= useSelector(({state}) => state.sidebar)
-  const {get, url}= useSelector(({state}) => state.server)
-  const [module, setModule]=useState([])
+  const {top, width} = useSelector(({state}) => state.sidebar)
+  const {get, url} = useSelector(({state}) => state.server)
+  const [module, setModule] = useState([])
   
   const getModule=()=>{
     get(url+`/dev/module/product`).then(res =>{
@@ -50,24 +50,22 @@ function Sidebar({className, setFilter}) {
 
   console.log(selections)
 
+  const firstLetterCapitalized = (str) => {
+    return str.charAt(0).toUpperCase() + str.substring(1)
+  }
+
   return (
-    <div style={{top:top,width:width}} className={`fixed left-0 h-full flex flex-col bg-stone-400 text-white z-10 ${className}`}>
+    <div style={{top:top,width:width}} className={`fixed left-0 h-full flex flex-col bg-stone-400 text-white z-10 ${className} overflow-y-auto`}>
 
-        <div classname="flex flex-col">
-          {/* se hace un map al modulo validator con su enum */}
-          {module.map((e,i)=>{
-            return <SidebarButton title={e.key} items={e.attributes.enum} onSelect={(selectedItems) => handleSelection(e.key,selectedItems)} />
-          })}
-        </div>
-
-        <button
-          className="py-2 px-4 bg-blue-500 text-white mt-12 hover:bg-blue-600"
-          onClick={handleClearFilters}
-        >
-        Clear Filters
-        </button>
+      <div classname="flex flex-col">
+        {/* se hace un map al modulo validator con su enum */}
+        {module.map((e,i)=>{
+          return <SidebarButton title={firstLetterCapitalized(e.key)} items={e.attributes.enum} onSelect={(selectedItems) => handleSelection(e.key, selectedItems)} />
+        })}
+      </div>
 
     </div>
+
 
     
   );
