bd983f590ce921175d928e8664c5fd3b93844d40
Jacobo Cohello <118378780+Jcooob@users.noreply.github.com>
2023-04-27 11:37:26 -0500

    scroll purchase modal

--- Client/src/components/UserInterface/PurchaseModal.jsx
+++ Client/src/components/UserInterface/PurchaseModal.jsx
@@ -3,22 +3,24 @@ const PurchaseModal = ({ purchase, onClose }) => {
     return (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-30 flex justify-center items-center">
         <div className="bg-white rounded-lg px-10 py-8 max-w-2xl flex flex-row">
-            <div className="w-1/2 flex-grow mr-24">
+            <div className="w-1/2 flex-grow mr-10">
                 <h3 className="text-2xl font-bold mb-6">Items bought</h3>
-                <ul className="divide-y divide-stone-300">
-                    {purchase.items.map((item) => (
-                    <li className="py-4" key={item.id}>
-                        <div className="flex items-center">
-                        <img src={item.image} alt={item.name} className="w-20 h-20 mr-6" />
-                        <div>
-                            <p className="font-bold">{item.name}</p>
-                            <p className="text-base">{item.amount} units</p>
-                            <p className="text-base">{item.total}</p>
-                        </div>
-                        </div>
-                    </li>
-                    ))}
-                </ul>
+                <div className="max-h-80 overflow-y-auto">
+                    <ul className="divide-y divide-stone-300">
+                        {purchase.items.map((item) => (
+                        <li className="py-4" key={item.id}>
+                            <div className="flex items-center">
+                            <img src={item.image} alt={item.name} className="w-20 h-20 mr-6" />
+                            <div>
+                                <p className="font-bold">{item.name}</p>
+                                <p className="text-base">{item.amount} units</p>
+                                <p className="text-base">{item.total}</p>
+                            </div>
+                            </div>
+                        </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div className="w-1/2">
                 <h2 className="text-2xl font-bold mb-6">Purchase Detail</h2>
