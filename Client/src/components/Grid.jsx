92ac27ef6da03cbb6ad594cfd335f4f9417c68bc
Maximo Guzman <vmaximoguzman@gmail.com>
2023-04-27 20:41:33 -0300

    Cards centradas

--- Client/src/components/Grid.jsx
+++ Client/src/components/Grid.jsx
@@ -1,28 +1,40 @@
+import styled from "styled-components";
 
-import styled from 'styled-components'
-
-export default function Grid({style,children, childHeight,childWidth,className}) {
-    
-
-    return (
-        <Container style={style} childHeight={childHeight} childWidth={childWidth} className={`min-h-[800px]`}>
-            {
-            children
-            }
-            
-        </Container>
-    )
+export default function Grid({
+  style,
+  children,
+  childHeight,
+  childWidth,
+  className,
+}) {
+  return (
+    <Container
+      style={style}
+      childHeight={childHeight}
+      childWidth={childWidth}
+      className={`min-h-[800px] place-items-center`}
+    >
+      {children}
+    </Container>
+  );
 }
 
 const Container = styled.div`
-display: grid;
-grid-template-columns: repeat(auto-fit, minmax(${props => props.childWidth? props.childWidth+"px": '200px'}, 1fr));
-grid-gap: 1rem;
-grid-template-rows: auto;
-justify-content: center;
-align-items: flex-start;
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(
+      ${(props) => (props.childWidth ? props.childWidth + "px" : "200px")},
+      1fr
+    )
+  );
+  grid-gap: 1rem;
+  grid-template-rows: auto;
+  justify-content: center;
+  align-items: flex-start;
 
-& .card{
-    height: ${props => props.childHeight? props.childHeight+"px": '260px'};
-}
-`
+  & .card {
+    height: ${(props) =>
+      props.childHeight ? props.childHeight + "px" : "260px"};
+  }
+`;
