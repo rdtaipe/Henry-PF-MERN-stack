7b82672f09d165b1636cebef25003b4f496bd88c
Renzo Cervantes <r33n84@gmail.com>
2023-04-29 22:01:42 -0300

    fix sales and purchase

--- server/src/controllers/statsController.js
+++ server/src/controllers/statsController.js
@@ -1,251 +1,114 @@
 import productModel from "../models/product.js";
+import purchaseModel from "../models/purchase.js";
+import userModel from "../models/user.js";
 
+/*  */ /*  */ /*  */
 export const getSales = async (req, res) => {
   const { month, year } = req.query;
-  try {
-    const startDate = new Date(year, month - 1, 1);
-    const endDate = new Date(year, month, 0);
+  const startDate = new Date(year, month - 1, 1);
+  const endDate = new Date(year, month, 0);
 
-    const data = fakeData.filter((purchase) => {
-      const purchaseDate = new Date(purchase.time);
-      return purchaseDate >= startDate && purchaseDate <= endDate;
+  try {
+    const data = await purchaseModel.find({
+      createdAt: { $gte: startDate, $lt: endDate },
     });
 
     const formatData = data.map((el) => {
-      const date = new Date(el.time);
-      const sold = el.products.reduce((accumulator, product) => {
-        return accumulator + product.price * product.quantity;
+      const total = el.products.reduce((accumulator, product) => {
+        return accumulator + product.price * product.total;
       }, 0);
       return {
-        day: date.getDate(),
-        sold,
+        total,
+        day: new Date(el.createdAt).getDate(),
       };
     });
 
-    const resp = [];
+    const addDays = [];
     for (let i = 1; i <= endDate.getDate(); i++) {
       const dayData = formatData.find((item) => item.day === i);
       if (dayData) {
-        resp.push(dayData);
+        addDays.push(dayData);
       } else {
-        resp.push({ day: i, sold: 0 });
+        addDays.push({ day: i, total: 0 });
       }
     }
 
-    res.json(resp);
+    const salesByDay = {};
+    for (const { day, total } of addDays) {
+      if (salesByDay[day]) {
+        salesByDay[day] += total;
+      } else {
+        salesByDay[day] = total;
+      }
+    }
+
+    res.json(salesByDay);
   } catch (error) {
     res.status(400).json(error.message);
   }
 };
 
+/*  */ /*  */ /*  */
 export const getPurchases = async (req, res) => {
   const { month, year } = req.query;
-
   const startDate = new Date(year, month - 1, 1);
   const endDate = new Date(year, month, 0);
 
-  const data = await productModel.find(
-    { createdAt: { $gte: startDate, $lt: endDate } },
-    { stock: 1, cost: 1, createdAt: 1 }
-  );
+  try {
+    const data = await productModel.find(
+      { createdAt: { $gte: startDate, $lt: endDate } },
+      { stock: 1, cost: 1, createdAt: 1 }
+    );
 
-  const formatData = data.map((el) => {
-    const day = new Date(el.createdAt).getDate();
-    const purchase = el.stock * el.cost;
-    return {
-      day,
-      purchase,
-    };
-  });
+    const formatData = data.map((el) => {
+      const day = new Date(el.createdAt).getDate();
+      const purchase = el.stock * el.cost;
+      return {
+        day,
+        purchase,
+      };
+    });
 
-  const resp = [];
-  for (let i = 1; i <= endDate.getDate(); i++) {
-    const dayData = formatData.find((item) => item.day === i);
-    if (dayData) {
-      resp.push(dayData);
-    } else {
-      resp.push({ day: i, purchase: 0 });
+    const addDays = [];
+    for (let i = 1; i <= endDate.getDate(); i++) {
+      const dayData = formatData.find((item) => item.day === i);
+      if (dayData) {
+        addDays.push(dayData);
+      } else {
+        addDays.push({ day: i, purchase: 0 });
+      }
     }
-  }
 
-  res.json(data);
+    const purchasesByDay = {};
+    for (const { day, purchase } of addDays) {
+      if (purchasesByDay[day]) {
+        purchasesByDay[day] += purchase;
+      } else {
+        purchasesByDay[day] = purchase;
+      }
+    }
+
+    res.json(purchasesByDay);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 };
 
-const fakeData = [
-  {
-    _id: "6167bdf83c3de3f3c8d92819",
-    paymentId: "1234567890",
-    userId: "user123",
-    products: [
-      {
-        productId: "prod123",
-        name: "Product Name",
-        quantity: 2,
-        price: 19.99,
-      },
-      {
-        productId: "prod456",
-        name: "Another Product",
-        quantity: 1,
-        price: 9.99,
-      },
-    ],
-    addressId: "addr123",
-    time: "2022-01-01T12:34:56.789Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92820",
-    paymentId: "0987654321",
-    userId: "user456",
-    products: [
-      {
-        productId: "prod789",
-        name: "Product Name",
-        quantity: 1,
-        price: 29.99,
-      },
-      {
-        productId: "prod012",
-        name: "Another Product",
-        quantity: 3,
-        price: 14.99,
-      },
-    ],
-    addressId: "addr456",
-    time: "2022-01-03T08:12:34.567Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92821",
-    paymentId: "1357908642",
-    userId: "user789",
-    products: [
-      {
-        productId: "prod345",
-        name: "Product Name",
-        quantity: 1,
-        price: 49.99,
-      },
-      {
-        productId: "prod678",
-        name: "Another Product",
-        quantity: 2,
-        price: 24.99,
-      },
-    ],
-    addressId: "addr789",
-    time: "2022-01-05T15:43:21.210Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92822",
-    paymentId: "2468135790",
-    userId: "user012",
-    products: [
-      {
-        productId: "prod901",
-        name: "Product Name",
-        quantity: 3,
-        price: 9.99,
-      },
-      {
-        productId: "prod234",
-        name: "Another Product",
-        quantity: 1,
-        price: 19.99,
-      },
-    ],
-    addressId: "addr012",
-    time: "2022-01-08T11:22:33.444Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92823",
-    paymentId: "3692581470",
-    userId: "user345",
-    products: [
-      {
-        productId: "prod567",
-        name: "Product Name",
-        quantity: 2,
-        price: 29.99,
-      },
-      {
-        productId: "prod890",
-        name: "Another Product",
-        quantity: 1,
-        price: 14.99,
-      },
-    ],
-    addressId: "addr345",
-    time: "2022-01-10T17:32:10.987Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92824",
-    paymentId: "4829103765",
-    userId: "user678",
-    products: [
-      {
-        productId: "prod123",
-        name: "Product Name",
-        quantity: 1,
-        price: 39.99,
-      },
-      {
-        productId: "prod456",
-        name: "Another Product",
-        quantity: 2,
-        price: 19.99,
-      },
-    ],
-    addressId: "addr678",
-    time: "2022-02-15T09:08:07.654Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92825",
-    paymentId: "9753108642",
-    userId: "user901",
-    products: [
-      {
-        productId: "prod789",
-        name: "Product Name",
-        quantity: 2,
-        price: 29.99,
-      },
-      {
-        productId: "prod012",
-        name: "Another Product",
-        quantity: 1,
-        price: 14.99,
-      },
-    ],
-    addressId: "addr901",
-    time: "2022-02-17T14:21:43.210Z",
-    state: "Delivered",
-  },
-  {
-    _id: "6167bdf83c3de3f3c8d92826",
-    paymentId: "8642097531",
-    userId: "user234",
-    products: [
-      {
-        productId: "prod345",
-        name: "Product Name",
-        quantity: 1,
-        price: 19.99,
-      },
-      {
-        productId: "prod678",
-        name: "Another Product",
-        quantity: 3,
-        price: 9.99,
-      },
-    ],
-    addressId: "addr234",
-    time: "2022-02-21T18:54:32.109Z",
-    state: "Delivered",
-  },
-];
+/*  */ /*  */ /*  */
+export const getUsersLog = async (req, res) => {
+  const {} = req.body;
+
+  const data = await userModel.find()
+
+  res.json(data)
+};
+
+
+/*  */  /*  */  /*  */
+export const getProductsBestValued =  async (req,res) => {
+
+  const data = await productModel.find()
+
+  const resp = data.map(el => el.stars)
+  res.json(resp)
+}
--- server/src/routes/statsRouter.js
+++ server/src/routes/statsRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import {getSales,getPurchases} from '../controllers/statsController.js'
+import {
+  getSales,
+  getPurchases,
+  getUsersLog,
+  getProductsBestValued,
+} from "../controllers/statsController.js";
 
-const router = express.Router()
+const router = express.Router();
 /////////////////////////////////////////////
-router.get('/sales', getSales)
-router.get('/purchases', getPurchases)
+router.get("/sales", getSales);
+router.get("/purchases", getPurchases);
+router.get("/users", getUsersLog);
+router.get("/bestproducts", getProductsBestValued);
 
 /////////////////////////////////////////////
 
-export default router
+export default router;
