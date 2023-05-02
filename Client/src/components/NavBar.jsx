f2cd642ed9c232e524570ffbb85bf04b84eaf5f0
Maximo Guzman <vmaximoguzman@gmail.com>
2023-05-02 12:51:58 -0300

    Navbar Fix Responsive

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -96,7 +96,7 @@ const NavBar = () => {
             onTrue={handleModal}
           />
         )}
-        <div className="relative flex justify-center w-[200px] p-5">
+        <div className="relative flex justify-center w-[200px] md:w-[160px] lg:w-[200px] p-5 md:p-3 lg:p-5">
           <NavLink to="/">
             <img src={logo} alt="logo" className="w-32" />
           </NavLink>
@@ -105,7 +105,7 @@ const NavBar = () => {
 
         <div
           style={{ width: `calc(100% - ${width}px)` }}
-          className={`flex-col items-center md:flex md:flex-row md:relative md:top-0 md:pb-0 md:gap-0  ${open
+          className={`flex-col items-center justify-start md:flex md:flex-row md:relative md:top-0 md:pb-0 md:gap-0  ${open
             ? "absolute top-[80px] bg-black !w-full pb-5 gap-5 flex"
             : "hidden !w-full"
             } ${location.pathname === "/home" ? "md:justify-around" : "md:justify-between"}`}
@@ -113,18 +113,18 @@ const NavBar = () => {
           <div className="flex items-center justify-center w-[160px] md:w-fit">
             <NavLink
               to="/home"
-              className="text-white mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap"
+              className="text-white mx-2 lg:mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap"
             >
               Home
             </NavLink>
             <NavLink
               to="/about"
-              className="text-white mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap"
+              className="text-white mx-2 lg:mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap"
             >
               About Us
             </NavLink>
             <NavLink to='/questions'
-              className="text-white mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap">
+              className="text-white mx-2 lg:mx-4 text-lg hover:text-stone-400 hover:transform transition-all duration-500 whitespace-nowrap">
               Questions
             </NavLink>
           </div>
