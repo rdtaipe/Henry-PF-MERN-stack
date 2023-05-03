6cbf1ea5b5e4ca55478dcb442737253c41b865be
Renzo Cervantes <r33n84@gmail.com>
2023-05-03 13:20:19 -0300

    work un dashboard

---
+++ dashboard/src/Components/ProductCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+
+const ProductCard = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: 120 }}>
+      <CardActionArea>
+        <a href="http://www.google.com" target="_blank">
+          <CardMedia
+            component="img"
+            height="100"
+            image={product.image[0]}
+            alt={product.name}
+          />
+          <CardContent>
+            <Typography fontSize={15} variant="p" component="div">
+              {product.name}
+            </Typography>
+            <Typography
+              fontSize={15}
+              variant="p"
+              component="div"
+              color={"blue"}
+            >
+              {product.stars} stars
+            </Typography>
+          </CardContent>
+        </a>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ProductCard;
