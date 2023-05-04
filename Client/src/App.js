badbaff4618de247d6c720d92e94e4d544d78045
DrewAce <juancarmona3090@gmail.com>
2023-05-04 11:18:23 -0500

    Ban Screen

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import InicialPage from "./pages/InicialPage";
-import { Route, Routes, useHref } from "react-router-dom";
+import { Route, Routes, useHref, useNavigate } from "react-router-dom";
 import { Detail } from "./pages/Detail";
 import Footer from "./components/Footer";
 import { About } from "./pages/About";
@@ -16,7 +16,7 @@ import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import NavBar from "./components/NavBar";
 import Questions from "./pages/Questions";
 import Button from '@mui/material/Button'
-
+import Error from "./pages/ErrorComponent/Error";
 const server = {
   local: "http://localhost:5000",
   production: "https://wgxjjo-5000.csb.app",
@@ -25,6 +25,7 @@ const server = {
 function App() {
   const [userStatus, setUserStatus] = useState()
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { setter, auth, url } = useSelector(({ state }) => state.server);
   const { data } = useSelector(({ state }) => state.user);
   const { isAuthenticated, logout,getAccessTokenSilently} = useAuth0();
@@ -41,15 +42,16 @@ function App() {
       auth.get(`${url}/users/find/${userData._id}`).then(res => setUserStatus(res.data.status))
     }
     if(userStatus === "inactive"){
-      alert("user ban")
-      logout()
+      navigate('/error')
+      // alert("user ban")
+      // logout()
     }
 
   }, [userStatus]);
 
   return (
     <div>
-      {page !== "/" && <NavBar />}
+      {page !== "/" && page != '/error' && <NavBar />}
       <Routes>
         <Route path="/" element={<InicialPage />} />
         <Route path="/home" element={<Home />} />
@@ -61,6 +63,7 @@ function App() {
         <Route path="/authorize" element={<Authorize />} />
         <Route path="/user" element={<User />} />
         <Route path="/questions" element={<Questions />} />
+        <Route path="/error" element={<Error/>}/>
       </Routes>
 
       {page !== "/" && <ScrollAnimate footer={<Footer />} />}
