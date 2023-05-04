6ba38202a67e5cac4171761a60054deacd3cf921
Renzo Cervantes <r33n84@gmail.com>
2023-05-03 23:46:44 -0300

    work in stats

--- dashboard/src/Pages/Dashboard/Charts/Products.js
+++ dashboard/src/Pages/Dashboard/Charts/Products.js
@@ -6,6 +6,7 @@ import { Box, Container, Stack, Typography } from "@mui/material";
 const Products = () => {
   const [bestproducts, setBestproducts] = useState();
   const [whitoutstock, setWhitoutstock] = useState();
+  const [moresold, setMoreSold] = useState();
 
   function fetchUsersGenres() {
     axios.get(`http://localhost:5000/stats/bestproducts`).then((res) => {
@@ -17,22 +18,52 @@ const Products = () => {
       setWhitoutstock(res.data);
     });
   }
+  function fetchMoreSold() {
+    axios.get(`http://localhost:5000/stats/moresold`).then((res) => {
+      setMoreSold(res.data);
+    });
+  }
+
   useEffect(() => {
     fetchUsersGenres();
     fetchWhitoutStock();
+    fetchMoreSold();
   }, []);
 
   return (
     <Container sx={{ mt: 5 }}>
-      <Typography>Best valued:</Typography>
-      <Stack direction="row" useFlexGap flexWrap="wrap" spacing={3}>
-        {bestproducts && bestproducts.map((el) => <ProductCard product={el} />)}
-      </Stack>
-      {whitoutstock && whitoutstock.length < 0 ? (
-        whitoutstock.map((el) => <ProductCard product={el} />)
-      ) : (
-        <Typography>No hay productos sin Stock</Typography>
-      )}
+      <Box>
+        <Typography>Best Valued:</Typography>
+        <Stack direction="row" useFlexGap flexWrap="wrap" spacing={3}>
+          {bestproducts &&
+            bestproducts.map((el) => (
+              <ProductCard product={el} string={"stars"} dato={el.stars} />
+            ))}
+        </Stack>
+      </Box>
+
+      <Box mt={5}>
+        <Typography>More Sold:</Typography>
+        <Stack direction="row" useFlexGap flexWrap="wrap" spacing={3}>
+          {moresold &&
+            moresold.map((el) => (
+              <ProductCard product={el} string={"sold"} dato={el.total} />
+            ))}
+        </Stack>
+      </Box>
+
+      <Typography mt={5}>Without Stock:</Typography>
+      <Box
+        sx={{ display: "flex", alignItems: "center" }}
+        mt={1}
+        minHeight={100}
+      >
+        {whitoutstock && whitoutstock.length < 0 ? (
+          whitoutstock.map((el) => <ProductCard product={el} />)
+        ) : (
+          <Typography ml={5}>No hay productos sin Stock</Typography>
+        )}
+      </Box>
     </Container>
   );
 };
--- server/src/controllers/statsController.js
+++ server/src/controllers/statsController.js
@@ -22,10 +22,40 @@ export const getSales = async (req, res) => {
   const endDate = new Date(year, month, 0, 23);
 
   try {
-    const data = await purchaseModel.find({
-      createdAt: { $gte: startDate, $lt: endDate },
+    const data = await purchaseModel.find({}, { products: 1 });
+
+    const products = [];
+    data.map((el) => {
+      el.products.map((el) =>
+        products.push({ date: el.date, total: el.price * el.total })
+      );
     });
 
+    const filterForMonth = products.filter(
+      (product) => new Date(product.date).getMonth() === month - 1
+    );
+
+    const totalForDay = {};
+    filterForMonth.forEach((obj) => {
+      const date = new Date(obj.date).getDate();
+      if (date in totalForDay) {
+        totalForDay[date] += obj.total;
+      } else {
+        totalForDay[date] = obj.total;
+      }
+    });
+
+    for (let i = 1; i <= endDate.getDate(); i++) {
+      if (!totalForDay[i]) {
+        totalForDay[i] = 0;
+      }
+    }
+
+    res.json(totalForDay);
+    /*     const data = await purchaseModel.find({
+      createdAt: { $gte: startDate, $lt: endDate },
+    });
+   console.log(data);
     const formatData = data.map((el) => {
       const total = el.products.reduce((accumulator, product) => {
         return accumulator + product.price * product.total;
@@ -55,7 +85,7 @@ export const getSales = async (req, res) => {
       }
     }
 
-    res.json(salesByDay);
+    res.json(salesByDay); */
   } catch (error) {
     res.status(400).json(error.message);
   }
@@ -159,6 +189,43 @@ export const getProductWithoutStock = async (req, res) => {
   try {
     const products = await productModel.find({ stock: { $eq: 0 } });
     res.json(products);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+/*  */ /*  */ /*  */
+export const getProductMoreSold = async (req, res) => {
+  try {
+    const data = await purchaseModel.find({}, { products: 1 });
+
+    const products = [];
+    data.map((el) => {
+      el.products.map((el) => products.push(el));
+    });
+
+    const countSold = {};
+    products.forEach((el) => {
+      if (countSold[el.productId]) {
+        countSold[el.productId] = {
+          total: countSold[el.productId].total + el.total,
+          name: el.name,
+          image: el.image,
+          id: el.productId
+        };
+      } else {
+        countSold[el.productId] = {
+          total: el.total,
+          name: el.name,
+          image: el.image,
+          id: el.productId
+        };
+      }
+    });
+
+    const resp = Object.values(countSold).sort((a, b) => b.total - a.total)
+
+    res.json(resp.slice(0, 5));
   } catch (error) {
     res.status(400).json(error.message);
   }
